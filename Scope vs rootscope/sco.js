e5bd88a31da09b2195b41bc82d230381ecebff7f
Sameer Hakke <48816084+SamHakke@users.noreply.github.com>
2020-05-19 19:52:41 +0530

    javascript file

---
+++ Scope vs rootscope/sco.js
@@ -0,0 +1,24 @@
+//create module
+var app=angular.module("myApp",[]);
+
+//create first controller
+app.controller("controller1",function ($scope,$rootScope) {
+
+	//local $scope is used within the controller1.
+	$scope.name="Sameer Hakke";
+
+	//global $rootscope used anywhere 
+	$rootScope.msg1="Hey......It's Controller1..";
+
+});
+
+//create second controller 
+app.controller("controller2",function ($scope,$rootScope) {
+
+	$scope.name="Omkar Jamdar";
+
+	//we already used 'msg1' as a variable name so change the variable name.
+	$rootScope.msg2="Hey......It's Controller2....";
+
+});
+//So,the 'scope' used for declare local variable and 'rrotScope' is used for global variable declaration.
